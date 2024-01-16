b2729d6eb335f7f604802ebff1ae71e192a583fc
Oleg Asimov <arounte@proton.me>
2024-01-16 23:41:15 +0700

    Create lumiere.rb

---
+++ Casks/l/lumiere.rb
@@ -0,0 +1,36 @@
+cask "lumiere" do
+  version "1.0.0"
+  sha256 "5cfccc41676546955d28317a213d8f2068aac9c5975a598e57ddb267016d24d9"
+
+  url "https://lumiere.pinig.in/files/lumiere-#{version}.dmg"
+  name "lumiere"
+  desc "Free cinema app designed with Electron"
+  homepage "https://lumiere.pinig.in"
+
+  livecheck do
+    url "https://lumiere.pinig.in/files/latest-mac.yml"
+    strategy :electron_builder
+  end
+
+  postflight do
+    system_command "codesign",
+                    args: ["--force", "--deep", "--sign", "-", File.join(ENV["HOME"], "../../Applications/Lumiere.app")],
+                    sudo: false
+    system_command "xattr",
+                    args: ["-d", "com.apple.quarantine", File.join(ENV["HOME"], "../../Applications/Lumiere.app")],
+                    sudo: false
+  end
+
+  depends_on macos: ">= :catalina"
+
+  app "Lumiere.app"
+
+  zap trash: [
+    "~/Library/Caches/com.lumiere.app",
+    "~/Library/Caches/com.lumiere.app.ShipIt",
+    "~/Library/Caches/lumiere-updater",
+    "~/Library/HTTPStorages/com.lumiere.app",
+    "~/Library/Preferences/com.lumiere.app.plist",
+    "~/Library/Saved Application State/com.lumiere.app.savedState"
+  ]
+end
