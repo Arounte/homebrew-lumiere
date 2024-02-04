8252605d8227e6fb86476cf95342bcafe0528a2a
Oleg Asimov <arounte@proton.me>
2024-02-04 15:45:13 +0700

    version 0.6.1

--- Casks/l/lumiere.rb
+++ Casks/l/lumiere.rb
@@ -1,6 +1,6 @@
 cask "lumiere" do
-  version "0.6.0"
-  sha256 "19365649066cd26ed390a1a86d0a7efd18ed948c1ef2bb7feedc73797b81025d"
+  version "0.6.1"
+  sha256 "ded2010183a9bf3c7b3fcd3fb564ad580df4787d54ef1277798e0b10c887cdad"
 
   url "https://github.com/Arounte/lumiere-releases/releases/download/v#{version}/lumiere-#{version}.dmg"
   name "lumiere"
